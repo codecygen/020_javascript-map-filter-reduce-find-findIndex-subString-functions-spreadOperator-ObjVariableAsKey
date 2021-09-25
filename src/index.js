0dda965418a6cd05ca91e932d49e2d968ae56ef9
gingercook <vahitaras@gmail.com>
2021-09-25 00:40:33 -0400

    Notes updated!

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ let numArray = [3, 56, 2, 48, 5];
 
 // Notice that in order for this to work, you dont put the Math.pow section
 // inside the curly braces just like in a normal function.
+// Otherwise it gives error.
 const squareRootArray = numArray.map(eachNum => 
     Math.pow(eachNum, 0.5)
 );
 
 console.log(squareRootArray);
-
-
-//Map -Create a new array by doing something with each item in an array.
 
 //Filter - Create a new array by keeping the items that return true.
 
