6d36cece1b62da150e7f262e10833f8cea1e35f4
gingercook <vahitaras@gmail.com>
2021-09-27 21:06:28 -0400

    3 functions added

--- src/index.js
+++ src/index.js
@@ -3,16 +3,37 @@ let numArray = [3, 56, 2, 48, 5];
 // Notice that in order for this to work, you dont put the Math.pow section
 // inside the curly braces just like in a normal function.
 // Otherwise it gives error.
-const squareRootArray = numArray.map(eachNum => 
-    Math.pow(eachNum, 0.5)
-);
+// ================================================
+// map function
+// ================================================
+const squareRootArray = numArray.map(eachNum => Math.pow(eachNum, 0.5));
 
 console.log(squareRootArray);
+// Output: Array(5) [ 1.7320508075688772, 7.483314773547883, 1.4142135623730951, 6.928203230275509, 2.23606797749979 ]
 
-//Filter - Create a new array by keeping the items that return true.
+// ================================================
+// reduce function
+// ================================================
 
-//Reduce - Accumulate a value by doing something to each item in an array.
+const concatenateNum = numArray.reduce((previousNum, currentNum) => previousNum.toString() + currentNum.toString());
 
-//Find - find the first item that matches from an array.
+console.log(concatenateNum);
+// Output: 3562485
+
+// ================================================
+// filter function
+// ================================================
+
+const bigNumArray = numArray.filter(eachNum => eachNum > 30);
+console.log(bigNumArray);
+// Output: Array [ 56, 48 ]
+
+// ================================================
+// find function
+// ================================================
+
+
+
+
 
 //FindIndex - find the index of the first item that matches.
